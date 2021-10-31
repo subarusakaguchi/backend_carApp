99d126ce72709dd0e85bfd71365e2af118b0dea8
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-10-31 18:45:02 -0300

    Initial part Done

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (req, res) => {
+    const { name, description } = req.body;
+
+    categoriesRepository.create({ name, description });
+
+    return res.status(201).json({ msg: 'Category was sucefully added' });
+});
+
+categoriesRoutes.get('/', (req, res) => {
+    const all = res.json(categoriesRepository.list());
+
+    return res.json(all);
+});
+
+export { categoriesRoutes };
