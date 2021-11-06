2ee0f19afeb58d432dbbef367aa2b271b70e416a
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-11-06 14:11:01 -0300

    Added more upload functions and api docuumentation initiated

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
