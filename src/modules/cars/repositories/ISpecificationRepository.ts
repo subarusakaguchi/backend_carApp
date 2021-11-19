cf4ddb2ec767eb32c99ce52a44d1b8845095cd9f
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2021-11-19 10:23:42 -0300

    Added more docs to swagger

--- src/modules/cars/repositories/ISpecificationRepository.ts
+++ src/modules/cars/repositories/ISpecificationRepository.ts
@@ -6,9 +6,9 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationRepository {
-    create({ name, description }: ICreateSpecificationDTO): void;
+    create({ name, description }: ICreateSpecificationDTO): Promise<void>;
 
-    findByName(name: string): Specification;
+    findByName(name: string): Promise<Specification>;
 }
 
 export { ISpecificationRepository, ICreateSpecificationDTO };
